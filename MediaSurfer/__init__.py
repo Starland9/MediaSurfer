7edb205856958264c896eb9bb76ab861b7ba3819
ARIFI/ <154688178+arifiios@users.noreply.github.com>
2025-11-25 17:18:21 +0100

    Update __init__.py

--- MediaSurfer/__init__.py
+++ MediaSurfer/__init__.py
@@ -1,1 +1,6 @@
-
+from .instagram_service import InsTaGrAM_ServIcE
+from .yt_service import Yt_ServIcE
+from .tiktok_service import TiKToK_ServIcE
+from .spotify_service import SpOtIfY_ServIcE
+from .facebook_service import FaCeBoOk_ServIcE
+from .pinterest_service import PiNteResT_ServIcE
